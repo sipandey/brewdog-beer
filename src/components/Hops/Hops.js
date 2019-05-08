b4852fe0b6d413b21510e88f86e2560d0767b74e
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-08 16:34:48 +0530

    hops and malts in components

--- src/components/Hops/Hops.js
+++ src/components/Hops/Hops.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
-import {Table, Button} from 'reactstrap';
+import { Table, Button } from 'reactstrap';
 class Hops extends Component {
 
+    state = {
+        hops: [],
+        hop: {}
+    }
+    
+    onStateChange(hop) {
+        let middleHopCanChange = true;
+        let endHopCanChange = true;
+        if (hop.state === 'IDLE') {
+            if (hop.add === 'start')
+                hop.state = 'DONE';
+            else if (hop.add === 'middle') {
+                this.state.hops.forEach(function (hop) {
+                    if (hop.add === 'start' && hop.state !== 'DONE') {
+                        middleHopCanChange = false;
+                    }
+                });
+                if (middleHopCanChange) hop.state = 'DONE';
+            }
+            else {
+                this.state.hops.forEach(hop => {
+                    if ((hop.add === 'start' && hop.state !== 'DONE') || (hop.add === 'middle' && hop.state !== 'DONE')) {
+                        endHopCanChange = false;
+                    }
+                    if (endHopCanChange) hop.state = 'DONE'
+                });
+            }
+        }
+        else if (hop.state === 'DONE') {
+            hop.state = 'IDLE';
+        }
+        this.setState({ hop: hop })
+    }
+
     render() {
+        this.state.hops = this.props.hops;
         return (
             <Table><tbody>{this.props.hops.map(function (hop, index) {
                 return (
@@ -10,10 +45,10 @@ class Hops extends Component {
                         <td>{hop.name}</td>
                         <td>{hop.add}</td>
                         <td>{hop.attribute}</td>
-                        <td><Button color="info">{hop.state}</Button></td>
+                        <td><Button color="info" onClick={() => this.onStateChange(hop)}>{hop.state}</Button></td>
                     </tr>
                 )
-            })}</tbody></Table>
+            }, this)}</tbody></Table>
         )
     }
 }
--- src/components/Malts/Malts.js
+++ src/components/Malts/Malts.js
@@ -3,6 +3,18 @@ import React, {Component} from 'react';
 import {Table, Button} from 'reactstrap';
 
 class Malts extends Component {
+
+    state = {
+        malts: this.props.malts,
+        malt: {}
+    }
+    
+    onStateChange(malt) {
+        if(malt.state === 'IDLE') malt.state = 'DONE';
+        else if(malt.state === 'DONE') malt.state = 'IDLE';
+        this.setState({malt: malt})
+    }
+
     render() {
         return(
             <Table><tbody>{this.props.malts.map(function(malt, index){
@@ -11,10 +23,10 @@ class Malts extends Component {
                         <td>{malt.name}</td>
                         <td>{malt.amount.value}</td>
                         <td>{malt.amount.unit}</td>
-                        <td><Button color="info">{malt.state}</Button></td>
+                        <td><Button color="info" onClick={() => this.onStateChange(malt)}>{malt.state}</Button></td>
                     </tr>
                 )
-            })}</tbody></Table>
+            }, this)}</tbody></Table>
         )
     }
 }
