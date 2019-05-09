b3440e49c6a0b4704ceef8357440ba31fb7fb814
Siddharth Pandey <siddharth.pandey06@gmail.com>
2019-05-09 09:45:52 +0530

    minor logic change hops

--- src/components/Hops/Hops.js
+++ src/components/Hops/Hops.js
@@ -1,42 +1,59 @@
 import React, { Component } from 'react';
-import { Table, Button } from 'reactstrap';
+import { Table, Button} from 'reactstrap';
 class Hops extends Component {
 
-    state = {
-        hops: this.props.hops ? this.props.hops : [],
-        hop: {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            hops: this.props.hops,
+            hop: {}
+        }
     }
 
     onStateChange(hop) {
-        let middleHopCanChange = true;
-        let endHopCanChange = true;
-        if (hop.state === 'IDLE') {
-            if (hop.add === 'start')
-                hop.state = 'DONE';
-            else if (hop.add === 'middle') {
-                this.state.hops.forEach(function (hop) {
-                    if (hop.add === 'start' && hop.state !== 'DONE') {
-                        middleHopCanChange = false;
+        this.setState({ hops: this.props.hops }, function(){
+            let middleHopCanChange = true;
+            let endHopCanChange = true;
+            if (hop.state === 'IDLE') {
+                if (hop.add === 'start')
+                    hop.state = 'DONE';
+                else if (hop.add === 'middle') {
+                    this.state.hops.forEach(function (hop) {
+                        if (hop.add === 'start' && hop.state !== 'DONE') {
+                            middleHopCanChange = false;
+                        }
+                    });
+                    if (middleHopCanChange) {
+                        hop.state = 'DONE';
                     }
-                });
-                if (middleHopCanChange) hop.state = 'DONE';
-            }
-            else {
-                this.state.hops.forEach(hop => {
-                    if ((hop.add === 'start' && hop.state !== 'DONE') || (hop.add === 'middle' && hop.state !== 'DONE')) {
-                        endHopCanChange = false;
+                }
+                else {
+                    this.state.hops.forEach(hop => {
+                        if ((hop.add === 'start' && hop.state !== 'DONE') || (hop.add === 'middle' && hop.state !== 'DONE')) {
+                            endHopCanChange = false;
+                        }
+                    });
+                    if (endHopCanChange) {
+                        hop.state = 'DONE';
                     }
-                    if (endHopCanChange) hop.state = 'DONE'
-                });
+                }
             }
-        }
-        else if (hop.state === 'DONE') {
-            hop.state = 'IDLE';
-        }
-        this.setState({ hop: hop })
+            else if (hop.state === 'DONE') {
+                hop.state = 'IDLE';
+            }
+            if(!middleHopCanChange || !endHopCanChange) {
+                alert('Middle cannot be done until all hops with START state are DONE and End cannot be done until all hops with START and MIDDLE state are DONE!')
+            }
+            this.setState({ hop: hop })
+        });
     }
-    
+
     render() {
+        if(this.props.hops.length === 0) {
+            return(
+                <div></div>
+            )
+        }
         return (
             <Table><tbody>{this.props.hops.map(function (hop, index) {
                 return (
